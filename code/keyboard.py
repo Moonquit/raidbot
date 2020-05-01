2346e54d4b6c30f22545231436435b085646e4d1
ALex-Smir <61351722+ALex-Smir@users.noreply.github.com>
2020-05-01 17:54:13 +0300

    Update keyboard.py

--- code/keyboard.py
+++ code/keyboard.py
@@ -3,6 +3,9 @@ from vk_api.keyboard import VkKeyboard
 label = f"STOP!{' '*22}vto.pe"
 
 menu = VkKeyboard()
+one_time = False
+
+#1 line (2 buttons)
 menu.add_button(
     label=label,
     color="positive"
@@ -13,14 +16,81 @@ menu.add_button(
 )
 
 menu.add_line()
+
+
+#2 line (4 buttons)
 menu.add_button(
     label=label,
-    color="primary"
-)
+    color="primary")
+
+menu.add_button(
+    label=label,
+    color="positive")
+
 
 menu.add_button(
     label=label,
     color="positive"
 )
+menu.add_button(
+    label=label,
+    color="negative"
+)
+
+menu.add_line()
+
+
+#3 line (4 buttons)
+menu.add_button(
+    label=label,
+    color="primary")
+
+menu.add_button(
+    label=label,
+    color="positive")
+
+
+menu.add_button(
+    label=label,
+    color="positive"
+)
+menu.add_button(
+    label=label,
+    color="negative"
+)
+
+menu.add_line()
+
+
+#4 line (4 buttons)
+menu.add_button(
+    label=label",
+    color="primary")
+
+menu.add_button(
+    label=label,
+    color="positive")
+
+
+menu.add_button(
+    label=label,
+    color="positive"
+)
+menu.add_button(
+    label=label,
+    color="negative"
+)
+
+menu.add_line()
+
+
+# 5 line (2 buttons)
+menu.add_button(
+    label=label,
+    color="primary")
+
+menu.add_button(
+    label=label,
+    color="positive")
 
 menu = menu.get_keyboard()
