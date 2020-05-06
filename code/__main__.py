f28b536ad178dfa2c7fbebda9e5dc79e7ed7272d
ALex-Smir <61351722+ALex-Smir@users.noreply.github.com>
2020-05-06 12:13:12 +0300

    Update __main__.py

--- code/__main__.py
+++ code/__main__.py
@@ -17,17 +17,20 @@ api = vk_dev.Api(
 lp = api >> vk_dev.LongPoll()
 
 
+global chats
+chats = 0
+
 @lp.message_new()
 async def flood(event, pl):
     """
     Flood if it was invited
     """
-    chats = 0
-
+    
     if (
         'action' in event.object.message and
         event.object.message.action.type == 'chat_invite_user'
     ): 
+        global chats
         chats += 1
         print(f'\033[31m[*]\033[0m New chat | Total chats: {chats}')
         while True:
